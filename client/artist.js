b6a3a63ec995747b27d89c73b027f13196ddc1af
afsahurrehman11 <afsahurrehman11@gmail.com>
2024-06-08 13:23:14 +0500

    made changes in frontend

--- client/artist.js
+++ client/artist.js
@@ -1,4 +1,4 @@
-import username from "./home.js";
+import { userName } from "./home.js";
 let reqAlbum;
 let postData = {
   album: {},
@@ -9,12 +9,12 @@ fetch("/api/artist").then((res) => {
   res.json().then((artist) => {
     name.innerHTML = artist[0].artistName;
     artist[0].albums.forEach((album, idx) => {
-      albums.innerHTML += `<li>${album.title}</li>`;
+      albums.innerHTML += <li>${album.title}</li>;
     });
     reqAlbum = artist[0];
     postData.album = reqAlbum.albums[0];
     artist[1].songs.forEach((song, idx) => {
-      songs.innerHTML += `<li>${song.title}</li>`;
+      songs.innerHTML += <li>${song.title}</li>;
     });
     profilePic.setAttribute("src", artist[0].profile_pic);
     loader.classList.add("remove");
@@ -29,9 +29,9 @@ let mainBody = document.querySelector(".mainDataBefore");
 let songs = document.querySelector(".songs");
 let albums = document.querySelector(".albumNames");
 let profilePic = document.querySelector(".profilePicture");
-//let usr = document.querySelector(".account")
-console.log(username);
-
+let user_ = document.querySelector(".account");
+console.log(userName);
+user_.textContent = userName;
 albums.addEventListener("click", (e) => {
   reqAlbum.albums.forEach((album, idx) => {
     if (album.title === e.target.textContent) {
@@ -56,7 +56,7 @@ albums.addEventListener("click", (e) => {
     .then((data) => {
       songs.innerHTML = "";
       data.songs.forEach((song) => {
-        songs.innerHTML += `<li>${song.title}</li>`;
+        songs.innerHTML += <li>${song.title}</li>;
       });
     })
     .catch((error) => {
--- client/home.js
+++ client/home.js
@@ -1,16 +1,27 @@
-let username = null; 
-let result = null; 
+// function call() {
+//   return fetch("/api/username").then((res) => {
+//     res.json().then((result) => {
+//       accountInfo.textContent = result.usr;
+//       console.log(result.usr);
+//       return result;
+//     });
+//   });
+// }
+// let userName = call().then((res) => {});
+let userName = null;
+let result = null;
 try {
-    let res = await fetch("/api/username");
-    result = await res.json();
-}   
-catch(err) {
-    console.log(err);
+  let res = await fetch("/api/username");
+  result = await res.json();
+} catch (err) {
+  console.log(err);
+  userName = user_name;
 }
 
-username = result.usr;
-let accountInfo = document.querySelector(".account")
-accountInfo.innerHTML = username;
+userName = result.usr;
+let accountInfo = document.querySelector(".account");
+accountInfo.innerHTML = userName;
 
-console.log(username);
-export default username;
+console.log(userName);
+
+export { userName };
